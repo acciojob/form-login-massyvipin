9a1546b1ebe50db7d131cfcb66b0277fa956c99e
massyvipin <166917148+massyvipin@users.noreply.github.com>
2024-09-19 22:49:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function getFormvalue() {
+	const form = document.getElementById('form1');
    const firstName = form.elements['fname'].value;
 	const lastName = form.elements['lname'].value;
 	alert(firstName + " " + lastName)
